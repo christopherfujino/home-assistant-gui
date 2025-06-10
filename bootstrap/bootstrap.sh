56a8839591c56acfe9c023367173d1ab87eab7e6
Christopher Fujino <christopherfujino@gmail.com>
2025-06-10 15:05:26 -0700

    change styling

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -1,17 +1,29 @@
 #!/usr/bin/env bash
 
-if [[ $(id -u) -ne 0 ]]; then
-	echo "Must run as root"
-	exit 1
+set -euo pipefail
+
+if [[ $(id -u) -eq 0 ]]; then
+  echo "Must not run as root"
+  exit 1
+fi
+
+if [[ ! -d '../ignore' ]]; then
+  mkdir ../ignore
+fi
+
+LOCAL_FONT_PATH='../ignore/font.zip'
+if [[ ! -f "$LOCAL_FONT_PATH" ]]; then
+  curl -L 'https://style64.org/file/C64_TrueType_v1.2.1-STYLE.zip' -o "$LOCAL_FONT_PATH"
+  unzip "$LOCAL_FONT_PATH" -d '../ignore'
 fi
 
 # D-Bus is required for bluetooth
-docker run -d \
-	--name homeassistant \
-	--privileged \
-	--restart=unless-stopped \
-	-e TZ=America/Los_Angeles \
-	-v "$PWD/config:/config" \
-	-v /run/dbus:/run/dbus:ro \
-	--network=host \
-	ghcr.io/home-assistant/home-assistant:stable
+sudo docker run -d \
+  --name homeassistant \
+  --privileged \
+  --restart=unless-stopped \
+  -e TZ=America/Los_Angeles \
+  -v "$PWD/config:/config" \
+  -v /run/dbus:/run/dbus:ro \
+  --network=host \
+  ghcr.io/home-assistant/home-assistant:stable
--- render.go
+++ render.go
@@ -4,28 +4,55 @@ import (
 	"fmt"
 	rl "github.com/gen2brain/raylib-go/raylib"
 	image_color "image/color"
-	"math"
 )
 
+// light blue
+//var color = image_color.RGBA{
+//	R: 0x70,
+//	G: 0xA4,
+//	B: 0xB2,
+//	A: 0xFF,
+//}
 var color = image_color.RGBA{
-	G: 0xFF,
+	R: 0x65,
+	G: 0x5E,
+	B: 0xB5,
 	A: 0xFF,
 }
+
+// dark blue
 var backgroundColor = image_color.RGBA{
+	R: 0x35,
+	G: 0x28,
+	B: 0x79,
 	A: 0xFF,
 }
 
-// This is only an 8-bit value, but store it in an uint16 so we can detect
-// overflow.
-var green uint16
+// #000000 black
+// #FFFFFF white
+// #68372B red
+// #70A4B2 light blue
+// #6F3D86 purple
+// #588D43 green
+// #352879 dark blue
+// #B8C76F yellow
+// #6F4F25 brown
+// #433900 dark brown
+// #9A6759 light red
+// #444444 dark grey
+// #6C6C6C mid grey
+// #9AD284 light green
+// #6C5EB5 mid blue
+// #959595 light grey
 
+const spacing = 0
 const size int32 = 32
 const width = 800
 const height = 480
 const fps = 30
 const gradientInterval float64 = 4.0 / fps
 
-var gradientVariable float64
+var font rl.Font
 
 func initRender(config Config) {
 	rl.InitWindow(width, height, "Home Assistant")
@@ -34,6 +61,8 @@ func initRender(config Config) {
 		rl.ToggleFullscreen()
 	}
 	rl.SetTargetFPS(fps)
+	// TODO normalize this path
+	font = rl.LoadFont("./ignore/C64_TrueType_v1.2.1-STYLE/fonts/C64_Pro_Mono-STYLE.otf")
 }
 
 func endRender() {
@@ -41,19 +70,12 @@ func endRender() {
 }
 
 func render(stats SensorStats) {
-	gradientVariable += gradientInterval
-	if gradientVariable > math.Pi*2 {
-		gradientVariable = gradientVariable - math.Pi*2
-	}
-	var gradientSin = math.Sin(gradientVariable)
-	green = uint16((gradientSin+1)*(0x7F-0x40)) + 0x40
-	// TODO delete to optimize
-	if green > 0xFF {
-		panic("math error!")
-	}
 	rl.BeginDrawing()
 	rl.ClearBackground(backgroundColor)
-	var tempText = fmt.Sprintf("Temperature:\t%v\u00B0\nIlluminance:\t%d lx\nBattery:\t%d%%\nConductivity:\t%d\u00B5S/cm\nMoisture:\t%d%%", stats.Temperature, stats.Illuminance, stats.Battery, stats.Conductivity, stats.Moisture)
-	rl.DrawText(tempText, 5, 5, size, image_color.RGBA{G: uint8(green), A: 0xFF})
+	const borderDepth = 30
+	rl.DrawRectangleLinesEx(rl.Rectangle{X: 0, Y: 0, Width: width, Height: height}, borderDepth, color)
+	var tempText = fmt.Sprintf(" *** Plantadore 25 ***\n\nTemperature:\t%vF\nIlluminance:\t%d lx\nConductivity:\t%d S/cm\nMoisture:\t%d%%", stats.Temperature, stats.Illuminance, stats.Conductivity, stats.Moisture)
+	rl.DrawTextEx(font, tempText, rl.Vector2{X: borderDepth + 5, Y: float32(borderDepth + font.BaseSize)}, float32(font.BaseSize), spacing, color)
+	//rl.DrawText(tempText, 5, 5, size, image_color.RGBA{G: uint8(green), A: 0xFF})
 	rl.EndDrawing()
 }
